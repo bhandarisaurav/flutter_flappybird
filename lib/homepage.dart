398121dcda90ecc994c68ad672c35919783c7166
bhandarisaurav <sbhandari@deerhold.org>
2021-07-17 23:20:20 -0400

    :sparkles: Added Current Score and Best Score layout

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,21 +9,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdYAxis = 0;
+  static double birdYAxis = 0;
   double time = 0;
   double height = 0;
-  double initialHeight = 0;
+  double initialHeight = birdYAxis;
+  bool gameHasStarted = false;
 
   void jump() {
-    initialHeight = birdYAxis;
-    Timer.periodic(Duration(milliseconds: 100), (timer) {
-      time += 0.05;
-      height = -4.9 * time * time + 5 * time;
+    setState(() {
+      time = 0;
+      initialHeight = birdYAxis;
+    });
+    print('The values are: ${[birdYAxis, time, height, initialHeight]}');
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + 3 * time;
       setState(() {
         birdYAxis = initialHeight - height;
       });
+      if (birdYAxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
     });
-    print('The values are: ${[birdYAxis, time, height, initialHeight]}');
   }
 
   @override
@@ -32,9 +44,15 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
-            flex: 2,
+            flex: 7,
             child: GestureDetector(
-              onTap: jump,
+              onTap: () {
+                if (gameHasStarted) {
+                  jump();
+                } else {
+                  startGame();
+                }
+              },
               child: AnimatedContainer(
                 alignment: Alignment(0, birdYAxis),
                 duration: Duration(milliseconds: 0),
@@ -43,9 +61,50 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          Container(
+            height: 15,
+            color: Colors.green,
+          ),
           Expanded(
             child: Container(
-              color: Colors.green,
+              color: Colors.brown,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "SCORE",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "0",
+                        style: TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "BEST",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "10",
+                        style: TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
