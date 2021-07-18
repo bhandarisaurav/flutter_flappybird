a5831ed7d650bde0d81c42a4a4766dbfa8a88463
bhandarisaurav <sbhandari@deerhold.org>
2021-07-17 23:12:04 -0400

    :zap: added homepage layout

---
+++ lib/bird.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class MyBird extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "lib/images/kusumakar.png",
+      width: 70,
+    );
+  }
+}
